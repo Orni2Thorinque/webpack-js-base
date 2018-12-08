8c53ad390cd48c19c1bf67a93756029f3451e88d
Orni2Thorinque <Orni2Thorinque@gmail.com>
2018-12-08 01:40:41 +0100

    [Fix]: Reconfigured lauch scripts

--- webpack.config.js
+++ webpack.config.js
@@ -3,15 +3,22 @@ const HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const fs = require('fs');
+const path = require('path');
 
 const manifest = JSON.parse(fs.readFileSync('./manifest.json', 'utf8'));
 
 module.exports = {
+  mode: 'development',
   watch: true,
   watchOptions: {
     aggregateTimeout: 300,
     poll: 1000,
     ignored: /node_modules/,
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 4000,
   },
   module: {
     rules: [
